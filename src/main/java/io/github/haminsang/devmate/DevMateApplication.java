61dc14f009a735c7cd5f39293bb7cf597f6545f1
Ha-minsang <gkalstkd@gmail.com>
2026-03-30 17:40:53 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/io/github/haminsang/devmate/DevMateApplication.java
@@ -0,0 +1,13 @@
+package io.github.haminsang.devmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMateApplication.class, args);
+	}
+
+}
