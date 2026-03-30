8401b708ba7135d9c03c47ffe99f9c7ebae398ea
Ha-minsang <gkalstkd@gmail.com>
2026-03-30 18:19:29 +0900

    chore: 프로젝트 초기 세팅

--- src/test/java/io/github/haminsang/devmate/DevMateApplicationTests.java
+++ src/test/java/io/github/haminsang/devmate/DevMateApplicationTests.java
@@ -2,9 +2,14 @@ package io.github.haminsang.devmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class DevMateApplicationTests {
+
+    @MockitoBean
+    JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
